97ca3e2525123f28eb75bf37220a57f23aeddc1f
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-04-02 23:18:26 -0400

    Debugging again

--- netlify-cms-auth-proxy/run.sh
+++ netlify-cms-auth-proxy/run.sh
@@ -6,6 +6,7 @@ CONFIG_PATH=/data/options.json
 
 export OAUTH_CLIENT_ID=$(jq --raw-output ".oauth_client_id" $CONFIG_PATH)
 export OAUTH_CLIENT_SECRET=$(jq --raw-output ".oauth_client_secret" $CONFIG_PATH)
+export OAUTH_DEBUG=$(jq --raw-output ".oauth_debug" $CONFIG_PATH)
 #REDIRECT_URL=$(jq --raw-output ".redirect_url" $CONFIG_PATH)
 #GIT_HOSTNAME=$(jq --raw-output ".git_hostname" $CONFIG_PATH)
 
